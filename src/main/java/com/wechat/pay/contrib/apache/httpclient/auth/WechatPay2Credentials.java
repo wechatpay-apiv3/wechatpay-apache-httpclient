c09bbae857f50f41780856e4678e0b4f7b6413d7
asdtiang <asdtiangxia@163.com>
2020-06-05 00:20:38 +0800

    Update WechatPay2Credentials.java
    支持图片上传

--- src/main/java/com/wechat/pay/contrib/apache/httpclient/auth/WechatPay2Credentials.java
+++ src/main/java/com/wechat/pay/contrib/apache/httpclient/auth/WechatPay2Credentials.java
@@ -1,27 +1,47 @@
 package com.wechat.pay.contrib.apache.httpclient.auth;
 
+
 import com.wechat.pay.contrib.apache.httpclient.Credentials;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.security.SecureRandom;
+
+import com.wechat.pay.contrib.apache.httpclient.auth.Signer;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+/**
+ * @author abel lee
+ * @create 2020-06-04 23:54
+ **/
 
 public class WechatPay2Credentials implements Credentials {
-  private static final Logger log = LoggerFactory.getLogger(WechatPay2Credentials.class);
+  private static final Logger log = LoggerFactory.getLogger(com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Credentials.class);
 
   private static final String SYMBOLS =
-      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   private static final SecureRandom RANDOM = new SecureRandom();
   protected String merchantId;
   protected Signer signer;
+  protected String fileUploadBoundary;
+
 
   public WechatPay2Credentials(String merchantId, Signer signer) {
     this.merchantId = merchantId;
     this.signer = signer;
+    this.fileUploadBoundary = "boundary";
+  }
+
+  public WechatPay2Credentials(String merchantId, Signer signer, String fileUploadBoundary) {
+    this.merchantId = merchantId;
+    this.signer = signer;
+    this.fileUploadBoundary = fileUploadBoundary;
   }
 
   public String getMerchantId() {
@@ -56,17 +76,17 @@ public class WechatPay2Credentials implements Credentials {
     Signer.SignatureResult signature = signer.sign(message.getBytes("utf-8"));
 
     String token = "mchid=\"" + getMerchantId() + "\","
-        + "nonce_str=\"" + nonceStr + "\","
-        + "timestamp=\"" + timestamp + "\","
-        + "serial_no=\"" + signature.certificateSerialNumber + "\","
-        + "signature=\"" + signature.sign + "\"";
+      + "nonce_str=\"" + nonceStr + "\","
+      + "timestamp=\"" + timestamp + "\","
+      + "serial_no=\"" + signature.certificateSerialNumber + "\","
+      + "signature=\"" + signature.sign + "\"";
     log.debug("authorization token=[{}]", token);
 
     return token;
   }
 
   protected final String buildMessage(String nonce, long timestamp, HttpUriRequest request)
-      throws IOException {
+    throws IOException {
     URI uri = request.getURI();
     String canonicalUrl = uri.getRawPath();
     if (uri.getQuery() != null) {
@@ -76,14 +96,31 @@ public class WechatPay2Credentials implements Credentials {
     String body = "";
     // PATCH,POST,PUT
     if (request instanceof HttpEntityEnclosingRequestBase) {
-      body = EntityUtils.toString(((HttpEntityEnclosingRequestBase) request).getEntity());
+      if(canonicalUrl.equals("/v3/merchant/media/upload")){
+        HttpEntityEnclosingRequestBase requestBase = (HttpEntityEnclosingRequestBase) request;
+        InputStream inputStream = requestBase.getEntity().getContent();
+        StringBuilder contentSb = new StringBuilder();
+        String line;
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        while ((line = br.readLine()) != null) {
+          contentSb.append(line);
+        }
+        String str = contentSb.toString();
+        inputStream.close();
+        String[] strings = str.split("--"+this.fileUploadBoundary);
+        String firstSplit = strings[1];
+        int indexStart = firstSplit.indexOf("{\"filename\":");
+        body = firstSplit.substring(indexStart).trim();
+      }else {
+        body = EntityUtils.toString(((HttpEntityEnclosingRequestBase) request).getEntity());
+      }
     }
-
     return request.getRequestLine().getMethod() + "\n"
-        + canonicalUrl + "\n"
-        + timestamp + "\n"
-        + nonce + "\n"
-        + body + "\n";
+      + canonicalUrl + "\n"
+      + timestamp + "\n"
+      + nonce + "\n"
+      + body + "\n";
   }
 
 }
+
