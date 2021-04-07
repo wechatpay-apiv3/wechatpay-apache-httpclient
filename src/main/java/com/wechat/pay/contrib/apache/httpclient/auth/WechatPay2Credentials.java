5fc3ecccf6663f00f378bba7400a216b348b968f
xy-peng <peng.xiaoyu@gmail.com>
2021-04-07 16:50:16 +0800

    fix: #42,#43 buildMessage()使用toString(entity, utf-8)从entity中获得请求报文

--- src/main/java/com/wechat/pay/contrib/apache/httpclient/auth/WechatPay2Credentials.java
+++ src/main/java/com/wechat/pay/contrib/apache/httpclient/auth/WechatPay2Credentials.java
@@ -2,16 +2,16 @@ package com.wechat.pay.contrib.apache.httpclient.auth;
 
 import com.wechat.pay.contrib.apache.httpclient.Credentials;
 import com.wechat.pay.contrib.apache.httpclient.WechatPayUploadHttpPost;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.security.SecureRandom;
-
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.client.methods.HttpRequestWrapper;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 
 public class WechatPay2Credentials implements Credentials {
   private static final Logger log = LoggerFactory.getLogger(WechatPay2Credentials.class);
@@ -81,7 +81,7 @@ public class WechatPay2Credentials implements Credentials {
     if (request.getOriginal() instanceof WechatPayUploadHttpPost) {
       body = ((WechatPayUploadHttpPost) request.getOriginal()).getMeta();
     } else if (request instanceof HttpEntityEnclosingRequest) {
-      body = EntityUtils.toString(((HttpEntityEnclosingRequest) request).getEntity());
+      body = EntityUtils.toString(((HttpEntityEnclosingRequest) request).getEntity(), StandardCharsets.UTF_8);
     }
 
     return request.getRequestLine().getMethod() + "\n"
